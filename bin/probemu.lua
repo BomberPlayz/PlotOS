a0927892a7e2004403948124092e7387d5cce4ea
BomberPlayz <49914603+BomberPlayz@users.noreply.github.com>
2020-01-22 20:02:47 +0100

    Update probemu.lua

--- bin/probemu.lua
+++ bin/probemu.lua
@@ -1,1 +1,11 @@
-a
+local com = require("component")
+local eln = com.ElnProbe
+print("?probe min, 0%")
+local min = io.read()
+print("?probe max, 100%")
+local max = io.read()
+print("searching for voltage...")
+print("type ~current voltage for input 1")
+local inp1 = io.read()
+print("type ~current voltage for input 2")
+local inp2 = io.read()
