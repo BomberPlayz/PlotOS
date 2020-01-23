c85b3d0894fd8d162b774c55c1d2d98cc7fda6e6
BomberPlayz <49914603+BomberPlayz@users.noreply.github.com>
2020-01-23 18:45:04 +0100

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -1,1 +1,7 @@
+local net = require("internet")
 
+for line in io.lines("/ver") do
+ if not line == net.request("https://raw.githubusercontent.com/BomberPlayz/PlotOS/master/ver") then
+  shell.execute("/home/posinst")
+ end
+end
