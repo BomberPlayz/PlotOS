3aa5f48adc57d4da64989f747aed9520c83158e7
BomberPlayz <49914603+BomberPlayz@users.noreply.github.com>
2020-01-23 18:27:23 +0100

    Update 94_updchc.lua

--- boot/94_updchc.lua
+++ boot/94_updchc.lua
@@ -1,4 +1,6 @@
 local net = require("internet")
 for line in io.lines("/ver") do
- if not line == net.request("")
+ if not line == net.request("https://raw.githubusercontent.com/BomberPlayz/PlotOS/master/ver") then
+  print("It is recommanded to update PlotOS from "..io.lines("/ver")[1].." to "..net.request("https://raw.githubusercontent.com/BomberPlayz/PlotOS/master/ver"))
+ end
 end
