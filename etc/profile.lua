70946d4d5f6dc7b08520fc644a240f47f24f7aa9
BomberPlayz <49914603+BomberPlayz@users.noreply.github.com>
2020-01-23 18:31:24 +0100

    Update profile.lua

--- etc/profile.lua
+++ etc/profile.lua
@@ -43,3 +43,10 @@ local home_shrc = shell.resolve(".shrc")
 if fs.exists(home_shrc) then
   loadfile(shell.resolve("source", "lua"))(home_shrc)
 end
+
+local net = require("internet")
+for line in io.lines("/ver") do
+ if not line == net.request("https://raw.githubusercontent.com/BomberPlayz/PlotOS/master/ver") then
+  print("It is recommanded to update PlotOS from "..io.lines("/ver")[1].." to "..net.request("https://raw.githubusercontent.com/BomberPlayz/PlotOS/master/ver"))
+ end
+end
