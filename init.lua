7feb03cda86786f19d920432a7c46c141c6deb9a
Patriik <61064909+PatriikPlays@users.noreply.github.com>
2024-04-27 10:24:00 +0200

    format files

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 do
-  local addr, invoke = computer.getBootAddress(), component.invoke
-  local function loadfile(file)
-    local handle = assert(invoke(addr, "open", file))
-    local buffer = ""
-    repeat
-      local data = invoke(addr, "read", handle, math.huge)
-      buffer = buffer .. (data or "")
-    until not data
-    invoke(addr, "close", handle)
-    return load(buffer, "=" .. file, "bt", _G)
-  end
-  while true do
-  local kernPath = loadfile("/bootloader.lua")()
-  loadfile(kernPath)(loadfile)
-  end
+    local addr, invoke = computer.getBootAddress(), component.invoke
+    local function loadfile(file)
+        local handle = assert(invoke(addr, "open", file))
+        local buffer = ""
+        repeat
+            local data = invoke(addr, "read", handle, math.huge)
+            buffer = buffer .. (data or "")
+        until not data
+        invoke(addr, "close", handle)
+        return load(buffer, "=" .. file, "bt", _G)
+    end
+    while true do
+        local kernPath = loadfile("/bootloader.lua")()
+        loadfile(kernPath)(loadfile)
+    end
 end
